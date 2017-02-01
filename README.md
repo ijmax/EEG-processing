1a065a4dc52e338d66ea1aa0da9d9de4e1d005eb
Isuru Jayarathne <isuru@Jayarathnes-Mac-mini-2.local>
2017-02-02 02:22:13 +0900

    binary class two feature scatter plot generator

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# EEG-processing
+# EEG processing
+
+#plotall.m
+
+plotall function plots all the channel signals containg 
+in the .edf (Europian Data Format) file which is generated 
+by EMOTIV EPOC+ headset.
+
+Input: Path of the .edf file (ex: <folder_name>/<file>.edf
+Output: Matrix of 14 channel data (14x<data_length>)      
+
+
+#scatter2dall.m
+
+This function generates set of scatter plots of all possible 
+combination of binary feature sets
+
+Input: param1: Data matrix with any number of features (mxn)  
+        param2: labels for the given data set(only two classes)
+        param3: integer valuse representing the section of     
+                plots to show                                  
+Output: Scatter plots with 20 maximum plots 
---
+++ scatter2dall.m
@@ -0,0 +1,57 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                               %
+% This function generates set of scatter plots of all possible  %
+% combination of binary feature sets                            %
+%                                                               %
+% Author: Isuru Jayarathne                                      %
+% Email: isuru.jayarathne@gmail.com                             %
+%                                                               %
+% Date_Created: 01/02/2016                                      %
+% Last_Modified: 02/02/2017                                     %
+%                                                               %
+% Input: param1: Data matrix with any number of features (mxn)  %
+%        param2: labels for the given data set(only two classes)%
+%        param3: integer valuse representing the section of     %
+%                plots to show                                  %
+% Output: Scatter plots with 20 maximum plots                   %
+%                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function scatter2dall(data,labels,page)
+
+n = size(data,2);
+
+% getting all the binary combination for the given data set
+comb = combnk(1:n,2);
+
+% split data set two two classes
+cls1 = data(find(labels==0),:);
+cls2 = data(find(labels==1),:);
+
+% number of plots to show in one figure
+plots = 20;
+
+% set last plot number
+maxplot = page*plots;
+
+if(maxplot>size(comb,1))
+    maxplot = size(comb,1);
+end
+
+% maximize the figure window
+figure('units','normalized','outerposition',[0 0 1 1]);
+
+for i = (page-1)*plots+1:maxplot
+    % set plot position and the title
+    subplot(5,4,i-(page-1)*plots);
+    title(strcat(num2str(comb(i,1)),' and ',num2str(comb(i,2))));
+    
+    % plot 1st class
+    scatter(cls1(:,comb(i,1)),cls1(:,comb(i,2)),'*r');
+    hold on;
+    % plot 2nd class
+    scatter(cls2(:,comb(i,1)),cls2(:,comb(i,2)),'ob');
+end
+ 
+end
